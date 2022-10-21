dd5a3cd1622f3ccf06212da1491049f51004c995
Mihaela8150 <114097144+Mihaela8150@users.noreply.github.com>
2022-10-21 20:27:19 +0300

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,36 @@
+const text = 1;
+let text_1 = true;
+// text ="hi";
+text_1 = "salut"
+
+let score = null;
+let a = 1;
+let plus = "+";
+let b = 2;
+score = a + b;
+
+let f = "hehe";
+
+let c = "juftjy"
+alert("salut")
+prompt("salut cum te cumesti")
+
+let nume_prenume = null;
+let nume = " mihaela ";
+let prenume = " bizganu ";
+let gap=' ';
+nume_prenume = nume +gap + prenume
+console.log(nume_prenume);
+console.log("nume");
+
+console.log( f);
+
+console.log(score);
+
+
+
+console.log(typeof text);
+console.log(text_1)
+
+
+
